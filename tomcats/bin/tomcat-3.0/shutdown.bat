ef9dcbc829b4c6b2847b2a717c4416d86fc809a7
Yuliya Astapova <YuliyaA@avi.com>
2016-01-11 09:41:28 -0500

    Removed tomcats

--- tomcats/bin/tomcat-3.0/shutdown.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-rem $Id: shutdown.bat,v 1.2 1999/12/05 17:02:41 harishp Exp $
-rem Startup batch file for tomcat server.
-call tomcat stop
-rem pause
--- tomcats/bin/tomcat-3.0/startup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-rem $Id: startup.bat,v 1.2 1999/12/05 17:02:42 harishp Exp $
-rem Startup batch file for tomcat servner.
-
-rem This batch file written and tested under Windows NT
-rem Improvements to this file are welcome
-call tomcat start  
-rem pause
